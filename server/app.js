9a4404371a949bc81bebd7c62c88d2c526c2aa9f
Ville Nättinen <villenattinen@gmail.com>
2024-12-13 17:42:19 +0200

    Add health check to deployment

--- server/app.js
+++ server/app.js
@@ -14,7 +14,8 @@ mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
@@ -30,6 +31,9 @@ app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.get('/health', (req, res) => {
+  res.status(200).send('ok')
+})
 
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
